23a37b47379cd754ca46f839f42cf86d5bdce61a
Elks <officialdjdestiny115@gmail.com>
2026-08-08 03:19:25 +0000

    added room booking system

--- guesthouse_system/settings.py
+++ guesthouse_system/settings.py
@@ -16,10 +16,9 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
-).split(",")
+ALLOWED_HOSTS = [
+    "guest-house-syf1.onrender.com",
+]
 
 # Application definition
 
